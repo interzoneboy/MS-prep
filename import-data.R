66a1d0e70f8c951fed1b015b2ba45294fc22c932
interzoneboy <mattthew@gmail.com>
2015-12-18 09:49:45 -0500

    Add filtering/norm/id_mangle code

--- import-data.R
+++ import-data.R
@@ -38,39 +38,46 @@ read_data_file <- function(fname, body=NULL){
     return(d)
 }
 
-transpose_and_filter <- function(d_in, id_cols, measure_cols,
-                                col_filters=NULL,
-                                row_filters=NULL) {
-    #names_and_mz <- d[,id_cols]
-    #data <- d[,measure_cols]
+transpose_filter_normalize <- function(d_in, id_cols, measure_cols,
+                                       id_formatter=NULL,
+                                       col_filters=NULL,
+                                       row_filters=NULL,
+                                       normalizers=NULL) {
 
     # Take only the column indexes of interest, that either id the row, or id the sample.
     #
+    d_sieve_mz_info <- d_in[,id_cols]
     d_pre <- d_in[,c(id_cols, measure_cols)]
     d_trans <- data.frame(t(d_pre), stringsAsFactors=F)
     d_trans <- data.frame(id=row.names(d_trans), d_trans, stringsAsFactors=F)
 
     d <- d_trans[(length(id_cols)+1):nrow(d_trans),]
+    if (!is.null(id_formatter)){
+        d$id <- id_formatter(d$id)
+    }
 
     data_cols <- data.frame(lapply(d[,2:ncol(d)], as.numeric))
 
     # Now filter stuff --------------------------------------
     #
-    #   filtering...
-
-
-    data_cols <- Reduce(function(f, x){return(f(x))}, col_filters
-
-
+    #   Filter the data_cols, using the list of col filtering functions provided as argument.
+    if (!is.null(col_filters)){
+        data_cols <- Reduce(function(x, f){return(f(x))}, col_filters, init=data_cols)
+    }
+    d <- data.frame(id=d$id, data_cols, stringsAsFactors=F)
+    if (!is.null(row_filters)){
+        d <- Reduce(function(x,f){f(x)}, row_filters, init=d)
+    }
 
     # Now do normalizing stuff ------------------------------
     #
     #   normalizing...
-
+    if (!is.null(normalizers)){
+        d <- Reduce(function(x,f){f(x)}, normalizers, init=d)
+    }
 
     # Process sample ID if necessary (for matching to aux sample info)
     #
-
     ret <- data.frame(id=d$id, data_cols_out, stringsAsFactors=F)
     return(ret)
 
--- test-pipeline.R
+++ test-pipeline.R
@@ -4,11 +4,12 @@ f1 <- function(x){return(x+1)}
 f2 <- function(x){return(x+2)}
 f3 <- function(x){return(x+3)}
 
-appl <- function(x,y){
-    ret <- function(x){
-        y(x())
-    }
-    return(ret)
+appl <- function(x,f){
+    #ret <- function(x){
+    #    y(x())
+    #}
+    #return(ret)
+    f(x)
 }
 
-bla <- Reduce(appl, c(f1, f2, f3), init=function(i){return(i)})
+bla <- Reduce(appl, c(f1, f2, f3), init=0)
